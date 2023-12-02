7d5a2d389147bd828ac5bd82ba4f03666b3b0f84
anakin-dabir <anakindabir@gmail.com>
2023-12-02 21:30:09 +0500

    Codux

--- src/_codux/boards/app/app.board.tsx
+++ src/_codux/boards/app/app.board.tsx
@@ -3,5 +3,13 @@ import App from '../../../App';
 
 export default createBoard({
     name: 'App',
-    Board: () => <App />,
+    Board: () => <div className={'bg-green-400'} />,
+    environmentProps: {
+        windowWidth: 1920,
+        windowHeight: 1080,
+        canvasWidth: 1583,
+        canvasHeight: 1032,
+        windowBackgroundColor: '#402d2d',
+    },
+    isSnippet: false,
 });
